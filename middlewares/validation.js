317f2c890c97e312568f66c35e3a914cc23d36cf
AnnaIndyukova <atigra13@gmail.com>
2024-07-19 19:17:03 +0300

    Deploy - fix bugs

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,13 +1,12 @@
-
-const { Joi, celebrate } = require('celebrate');
-const validator = require('validator');
+const { Joi, celebrate } = require("celebrate");
+const validator = require("validator");
 
 const validateURL = (value, helpers) => {
   if (validator.isURL(value)) {
     return value;
   }
-  return helpers.error('string.uri');
-}
+  return helpers.error("string.uri");
+};
 
 module.exports.validateCardBody = celebrate({
   body: Joi.object().keys({
@@ -46,29 +45,26 @@ module.exports.validateUserBody = celebrate({
       "string.empty": 'The "password" field must be filled in',
     }),
   }),
-
-  module.exports.validateUserLogin = celebrate({
-    body: Joi.object().keys({
-      email: Joi.string().required().email().messages({
-        "string.empty": 'The "email" field must be filled in',
-        "string.email": 'the "email" field must be a valid email',
-      }),
-  
-      password: Joi.string().required().messages({
-        "string.empty": 'The "password" field must be filled in',
-      }),
-    }),
-  });
-
-  module.exports.validateID = celebrate({
-    params: Joi.object().keys({
-      ID: Joi.string().hex().length(24).messages({
-        "string.hex": 'the "ID" field must be a a hexadecimal value',
-        "string.length": 'The  length of the "ID" field must be 24',
-      }),
-  
-    }),
-  });
 });
 
+module.exports.validateUserLogin = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email().messages({
+      "string.empty": 'The "email" field must be filled in',
+      "string.email": 'the "email" field must be a valid email',
+    }),
 
+    password: Joi.string().required().messages({
+      "string.empty": 'The "password" field must be filled in',
+    }),
+  }),
+});
+
+module.exports.validateID = celebrate({
+  params: Joi.object().keys({
+    ID: Joi.string().hex().length(24).messages({
+      "string.hex": 'the "ID" field must be a a hexadecimal value',
+      "string.length": 'The  length of the "ID" field must be 24',
+    }),
+  }),
+});
